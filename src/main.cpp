7ce041f52311f77a858051c3161f9cf06aabea8d
kneecapFolder <ilan7gr@gmail.com>
2024-04-17 11:50:16 +0300

    created a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,18 @@
 #include <raylib.h>
-#include "ball.h"
 
-int main()
-{
-    Color darkGreen = Color{20, 160, 133, 255};
-
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    Ball ball = Ball();
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
+int main() {
+    InitWindow(800, 800, "Snake Game");
+    SetTargetFPS(3);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
+
+        ClearBackground(BLACK);
+
         EndDrawing();
     }
+    
 
     CloseWindow();
     return 0;
